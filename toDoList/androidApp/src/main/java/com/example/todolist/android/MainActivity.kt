12dd466d3a6e07b5fdd57689e2595fbb0d012c6c
Kim van Rijswijk <kim.van@etu.unilim.fr>
2024-04-01 17:59:13 +0200

    ajout du layout de la page principale

--- toDoList/androidApp/src/main/java/com/example/todolist/android/MainActivity.kt
+++ toDoList/androidApp/src/main/java/com/example/todolist/android/MainActivity.kt
@@ -8,25 +8,30 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.fragment.app.FragmentActivity
 import com.example.todolist.Greeting
+import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MyApplicationTheme {
+        //setContent {
+            /*MyApplicationTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     GreetingView(Greeting().greet())
                 }
-            }
-        }
-    }
-}
+            }*/
+        setContentView(R.layout.activity_main)
+    //}
+}}
 
-@Composable
+/*@Composable
 fun GreetingView(text: String) {
     Text(text = text)
 }
@@ -37,4 +42,5 @@ fun DefaultPreview() {
     MyApplicationTheme {
         GreetingView("Hello, Android!")
     }
-}
+}*/
+
