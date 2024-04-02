f8f29f7e25c9bd4f88622b38f822aa18768a2fef
Kim van Rijswijk <kim.van@etu.unilim.fr>
2024-04-02 22:24:52 +0200

    ajout de l'activité permettant de lister toutes les tâches

---
+++ toDoList/androidApp/src/main/java/com/example/todolist/android/ListTasksActivity.kt
@@ -0,0 +1,11 @@
+package com.example.todolist.android
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ListTasksActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_tasks)
+    }
+}
