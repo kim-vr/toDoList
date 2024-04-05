f02a8444a39c6f58fc9cf96091be5fa6ae66eb79
Kim van Rijswijk <kim.van@etu.unilim.fr>
2024-04-05 23:52:40 +0200

    ajout de la selection de date

--- toDoList/androidApp/src/main/java/com/example/todolist/android/AddTaskDialogFragment.kt
+++ toDoList/androidApp/src/main/java/com/example/todolist/android/AddTaskDialogFragment.kt
@@ -1,10 +1,15 @@
 package com.example.todolist.android
 
+import android.app.DatePickerDialog
 import android.app.Dialog
 import android.os.Bundle
 import androidx.fragment.app.DialogFragment
 import androidx.appcompat.app.AlertDialog
 import android.view.LayoutInflater
+import android.widget.EditText
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class AddTaskDialogFragment : DialogFragment() {
 
@@ -12,6 +17,22 @@ class AddTaskDialogFragment : DialogFragment() {
         val builder = AlertDialog.Builder(requireActivity())
         val inflater = requireActivity().layoutInflater
         val view = inflater.inflate(R.layout.add_task_layout, null)
+
+        val dateInput = view.findViewById<EditText>(R.id.date_input)
+        val descriptionInput = view.findViewById<EditText>(R.id.description_input)
+
+        //Permet d'ouvrir un DatePickerDialog
+        dateInput.setOnClickListener {
+            val calendar = Calendar.getInstance()
+            DatePickerDialog(requireContext(), { _, year, month, dayOfMonth ->
+                //Formate et affiche la date sélectionnée dans le champ de texte de date
+                val selectedDate = Calendar.getInstance()
+                selectedDate.set(year, month, dayOfMonth)
+                val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+                dateInput.setText(dateFormat.format(selectedDate.time))
+            }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show()
+        }
+
         builder.setView(view)
             .setTitle("Ajouter une tâche")
             .setPositiveButton("Ajouter") { dialog, id ->
@@ -22,4 +43,6 @@ class AddTaskDialogFragment : DialogFragment() {
             }
         return builder.create()
     }
+
+
 }
